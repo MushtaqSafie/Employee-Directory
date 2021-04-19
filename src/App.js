390ce28d7601633c19b8b7e1655401eba1c0553b
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-04-20 05:41:36 +1000

    installing react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Employee Directory
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
